1b681252a16b8d36c3aa05a286beb7e8abd99536
Ivander Destian Luis <ivanderluis7@gmail.com>
2024-05-31 08:42:38 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: PostingScreen(),
+      home: ProfileScreen(),
       initialRoute: '/',
       routes: {
         '/browse_screen': (context) => const BrowseScreen(),
--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -1,4 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flux/color_pallete.dart';
+import 'package:flux/models/account.dart';
+import 'package:flux/services/account_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -8,8 +14,31 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  late ColorPallete colorPallete;
+  late Account account;
+  late
+  late SharedPreferences prefs;
+  bool _isLoading = true;
+
+  void initialize() async {
+    prefs = await SharedPreferences.getInstance().then((value) async {
+      colorPallete = value.getBool('isDarkMode') ?? false
+          ? DarkModeColorPallete()
+          : LightModeColorPallete();
+      account = (await AccountService.getAccountByUid(
+          FirebaseAuth.instance.currentUser!.uid))!;
+
+      setState(() {
+        _isLoading = false;
+      });
+      return value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return _isLoading
+        ? const Center(child: CircularProgressIndicator())
+        : Scaffold(body: ,);
   }
 }
